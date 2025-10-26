bd67908f9ae6a3211096a5a850aefc657eed2859
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-10-26 07:49:37 +0100

    build(packaging): 📦 add packaging/publish scripts and bump versions to `1.0.0`
    * Add packaging and publish helpers:
      * `Build/Build-Package.ps1` - invoke release build with code signing
      * `Build/Publish-PackageGitHub.ps1` - publish GitHub release asset
      * `Build/Publish-PackageNuget.ps1` - publish NuGet package
      * `Build/Update-Version.ps1` - helper to inspect and set project versions
    * Prepare project for 1.0.0 release:
      * Bump `VersionPrefix` to `1.0.0` in `FileInspectorX/FileInspectorX.csproj` and `FileInspectorX.PowerShell/FileInspectorX.PowerShell.csproj`
      * Update module `ModuleVersion` to `1.0.0` in `Module/Build/Build-Module.ps1` and `Module/FileInspectorX.psd1`
    * Motivation: streamline packaging/publishing and mark the stable `1.0.0` release.

---
+++ Build/Publish-PackageGitHub.ps1
@@ -0,0 +1,11 @@
+﻿$GitHubAccessToken = Get-Content -Raw 'C:\Support\Important\GithubAPI.txt'
+
+$publishGitHubReleaseAssetSplat = @{
+    ProjectPath          = "$PSScriptRoot\..\FileInspectorX"
+    GitHubAccessToken    = $GitHubAccessToken
+    GitHubUsername       = "EvotecIT"
+    GitHubRepositoryName = "FileInspectorX"
+    IsPreRelease         = $false
+}
+
+Publish-GitHubReleaseAsset @publishGitHubReleaseAssetSplat
---
+++ Build/Update-Version.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module PSPublishModule -Force
+
+Get-ProjectVersion -Path "C:\Support\GitHub\FileInspectorX" -ExcludeFolders @('C:\Support\GitHub\FileInspectorX\Module\Artefacts') | Format-Table
+
+Set-ProjectVersion -Path "C:\Support\GitHub\FileInspectorX" -NewVersion "1.0.0" -Verbose -ExcludeFolders @('C:\Support\GitHub\FileInspectorX\Module\Artefacts') #-WhatIf
