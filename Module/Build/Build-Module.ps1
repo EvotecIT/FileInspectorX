ce8f2109bbdd35cd00933d92e2a05d5e99b41103
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-11-18 09:28:57 +0100

    chore(version): 🔖 update version to 1.0.3
    * Updated version numbers in project files to reflect the new release.
    * Ensured consistency across `FileInspectorX.PowerShell.csproj`, `FileInspectorX.csproj`, `Build-Module.ps1`, and `FileInspectorX.psd1`.

--- Module/Build/Build-Module.ps1
+++ Module/Build/Build-Module.ps1
@@ -1,7 +1,7 @@
 Build-Module -ModuleName 'FileInspectorX' {
     # Usual defaults as per standard module
     $Manifest = [ordered] @{
-        ModuleVersion        = '1.0.2'
+        ModuleVersion        = '1.0.3'
         CompatiblePSEditions = @('Desktop', 'Core')
         GUID                 = 'bb5de776-1f68-4af0-8d68-5c0fa2ab3cf9'
         Author               = 'Przemyslaw Klys'
