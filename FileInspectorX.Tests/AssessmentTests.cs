2220dc0668f180d3a53290b27b6579af7d9cee1c
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-03-12 18:45:26 +0100

    Extend unsigned signature penalty to ocx and scr

--- FileInspectorX.Tests/AssessmentTests.cs
+++ FileInspectorX.Tests/AssessmentTests.cs
@@ -343,6 +343,40 @@ public class AssessmentTests
     }
 
     [Fact]
+    public void Assess_Unsigned_Ocx_File_Gets_Signature_Absent_Penalty()
+    {
+        var analysis = new FileAnalysis
+        {
+            Detection = new ContentTypeDetectionResult
+            {
+                Extension = "ocx"
+            }
+        };
+
+        var assessed = FileInspector.Assess(analysis);
+
+        Assert.Equal(10, assessed.Score);
+        Assert.Contains("Sig.Absent", assessed.Codes);
+    }
+
+    [Fact]
+    public void Assess_Unsigned_Scr_File_Gets_Signature_Absent_Penalty()
+    {
+        var analysis = new FileAnalysis
+        {
+            Detection = new ContentTypeDetectionResult
+            {
+                Extension = "scr"
+            }
+        };
+
+        var assessed = FileInspector.Assess(analysis);
+
+        Assert.Equal(10, assessed.Score);
+        Assert.Contains("Sig.Absent", assessed.Codes);
+    }
+
+    [Fact]
     public void Assess_Unsigned_Msi_File_Gets_Signature_Absent_Penalty()
     {
         var analysis = new FileAnalysis
