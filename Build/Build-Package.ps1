e8a303e2f06e249cbf49db7440ca17d3e4f1bf33
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-03-16 17:50:24 +0100

    Remove legacy helper build scripts

--- Build/Build-Package.ps1
+++
@@ -1,1 +0,0 @@
-Invoke-DotNetReleaseBuild -ProjectPath "$PSScriptRoot\..\FileInspectorX" -CertificateThumbprint '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
--- Build/Publish-PackageNuget.ps1
+++
@@ -1,4 +0,0 @@
-Import-Module PSPublishModule -Force -ErrorAction Stop
-
-$NugetAPI = Get-Content -Raw -LiteralPath "C:\Support\Important\NugetOrgEvotec.txt"
-Publish-NugetPackage -Path "$PSScriptRoot\..\FileInspectorX\bin\Release" -ApiKey $NugetAPI
